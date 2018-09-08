c55ce84c25ba4ceef73f37ff89a9370496ca759e
Shubham Verma <shubhamv8527@gmail.com>
2018-09-08 23:18:56 +0530

    added router to route the project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Link,Switch } from 'react-router-dom'
+
 import './App.css';
 
+import InputSearch from './components/inputSearch';
+import SearchList from './components/SearchList/searchList';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={InputSearch} />
+
+          <Route path="/search/:term" component={SearchList} />
+          {/* <Route component={NoMatch} /> */}
+        </Switch>
+      </BrowserRouter>
+
+
     );
   }
 }
