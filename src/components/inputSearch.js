b1d745885ee43f2fe0830e666b07a7590f3dad79
Shubham Verma <shubhamv8527@gmail.com>
2018-09-09 12:26:47 +0530

    added error message for empty form submit

--- src/components/inputSearch.js
+++ src/components/inputSearch.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import { Input, Container,Header,Form,Icon } from 'semantic-ui-react';
-import { createBrowserHistory } from 'history';
+import { Input, Container,Header,Form,Icon,Message } from 'semantic-ui-react';
 
 import './inputSearch.css';
 
@@ -8,27 +7,41 @@ class InputSearch extends Component {
     constructor(){
         super();
         this.state = {
-
+            error:false
         };
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit = (e) => {
         console.log(e.target.searchTerm.value);
-        this.props.history.push('/search/'+e.target.searchTerm.value);
+        if(e.target.searchTerm.value<=0){
+            this.setState({error:true});
+        }
+        else{
+            this.setState({error:false});
+            this.props.history.push('/search/'+e.target.searchTerm.value);
+
+        }
     }
 
     render() {
         return (
             <div>
-                <Container style={{ marginTop: '3em',width:"50%" }}>
+                <Container style={{ margin: '0',
+                paddingTop:'20rem',
+                width:"50%" }}>
                 <Header as='h1' content='GITSEARCH' className='inputh1' textAlign='center' />
                 <Header as='h5' content='Search all git repo.Simply enter keyword such as Google,node' className='inputh2' textAlign='center' />
-                <Form onSubmit={this.handleSubmit}>
+                <Form onSubmit={this.handleSubmit} error={this.state.error}>
                     <Input icon={<Icon name='search'/>}
                         fluid
                         name='searchTerm'
                         placeholder='Search here git repository '
+                        />
+                        <Message
+                            error
+                        header='Input Field Is Empty'
+                        content='Please Enter Keyword to search the repository'
                         />
                 </Form>
                 </Container>
